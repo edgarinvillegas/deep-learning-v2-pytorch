1c7a87b13b333d7000ebcae982500e80f752b9f8
Edgar Villegas <edgar.villegas@coxautoinc.com>
2019-10-02 20:11:59 -0400

    feat Part2: Finish part2. Add version with OrderedDict to playground

--- _myplayground/Part2-Neural_networks_in_pytorch.py
+++ _myplayground/Part2-Neural_networks_in_pytorch.py
@@ -1,10 +1,16 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from collections import OrderedDict
 
 def resetWeight(layer):
     layer.weight.data.fill_(0)
     layer.bias.data.fill_(0)
+
+def resetWeights(model):
+    resetWeight(model.fc1)
+    resetWeight(model.fc2)
+    resetWeight(model.fc3)
 
 ## Your solution here
 class FourLayerNetwork(nn.Module):
@@ -19,26 +25,28 @@ class FourLayerNetwork(nn.Module):
         x = F.softmax(self.fc3(x), dim=1)
         return x
 flNetwork = FourLayerNetwork()
+resetWeights(flNetwork)
 
-resetWeight(flNetwork.fc1)
-resetWeight(flNetwork.fc2)
-resetWeight(flNetwork.fc3)
-
-flsNetwork = nn.Sequential(
-    nn.Linear(784, 128), nn.ReLU(),
-    nn.Linear(128, 64), nn.ReLU(),
-    nn.Linear(64, 10), nn.Softmax(dim=1)
-);
-resetWeight(flsNetwork[0])
-resetWeight(flsNetwork[2])
-resetWeight(flsNetwork[4])
+flsNetwork = nn.Sequential(OrderedDict([
+    ('fc1', nn.Linear(784, 128)),
+    ('relu1', nn.ReLU()),
+    ('fc2', nn.Linear(128, 64)),
+    ('relu2', nn.ReLU()),
+    ('fc3', nn.Linear(64, 10)),
+    ('softmax' , nn.Softmax(dim=1))
+]))
+resetWeights(flsNetwork)
 
 x = torch.randn((64, 784))
 outputCls = flNetwork.forward(x)
 outputSeq = flsNetwork.forward(x)
+
+#print('outputCls: ', outputCls)
+#print('outputSeq: ', outputSeq)
 
 print(outputCls.shape)
 print(outputSeq.shape)
 
 # Check that we get same output in both types of networks
 assert (outputCls != outputSeq).sum() == 0
+print('Success!')
